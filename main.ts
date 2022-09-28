e3020203bf50daa4cc364fc6c382fd3d0795235b
Niamh-Keenan <113034875+Niamh-Keenan@users.noreply.github.com>
2022-09-28 09:12:05 -0400

    added comment

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+// Created_by: Niamh_Keenan
+// Created_on: September_28_2022
+// 
+// This_program_controls_the_hunger_of_a_tamagotchi 
 let hungryness = 0
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
